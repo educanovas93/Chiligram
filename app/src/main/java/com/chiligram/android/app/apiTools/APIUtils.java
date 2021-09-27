337e01aa0cba9ee1081546a791c8633d9585feac
educanovas93 <educanovas93@gmail.com>
2021-09-27 13:56:31 +0200

    Update APIUtils.java
    cambio para el server de matrix

--- app/src/main/java/com/chiligram/android/app/apiTools/APIUtils.java
+++ app/src/main/java/com/chiligram/android/app/apiTools/APIUtils.java
@@ -4,8 +4,8 @@ package com.chiligram.android.app.apiTools;
 public class APIUtils {
 
 
-    public static final String BASE_URL = "https://matrix.cablehogar.es";
-    public static final String DOWNLOAD_SERVER = "matrix.cablehogar.es";
+    public static final String BASE_URL = "https://matrix.org";
+    public static final String DOWNLOAD_SERVER = "matrix.org";
 
 
     /*
